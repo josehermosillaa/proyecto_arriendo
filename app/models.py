934d2816d153a9446b2965eca6d5edd50f550df8
josehermosillaa <jose.hermosillaa@usach.cl>
2024-05-25 10:09:55 -0400

    add validators etcA

--- app/models.py
+++ app/models.py
@@ -1,33 +1,51 @@
 from django.db import models
 from django.contrib.auth.models import User 
 from django.core.validators import MinValueValidator
+from django.core.exceptions import ValidationError
 # Create your models here.
 
-class Usuario(models.Model):
+def validate_min_value(value):
+    if value < 0:
+        raise ValidationError(
+            'El valor debe ser positivo'
+        )
+class Usuario(models.Model):                                       
     """ este usuario es el que se usa para registrarse, loggearse etc auth.User"""
     #PK autofield
     id_usuario = models.ForeignKey('auth.User', on_delete=models.CASCADE)
 
-
+    def __str__(self):
+        return self.id_usuario.username
+    
 class Inmueble(models.Model): 
-    #P
+    
+
     id_usuario = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     id_tipo_inmueble = models.ForeignKey('Tipo_inmueble', on_delete=models.CASCADE)
     id_comuna = models.ForeignKey('Comuna', on_delete=models.CASCADE)
     id_region = models.ForeignKey('Region', on_delete=models.CASCADE)
     nombre_inmueble = models.CharField(max_length=200)
     m2_construido = models.FloatField()
-    numero_banos = models.IntegerField(validators= [MinValueValidator(0)],default=0)
-    numero_hab = models.IntegerField(validators= [MinValueValidator(0)],default=0)
+    numero_banos = models.PositiveIntegerField(default=0)
+    numero_hab = models.IntegerField(validators=[validate_min_value],default=0)
     direccion = models.CharField(max_length=200)
-
+    
+    def __str__(self):
+        return self.nombre_inmueble
+    
 class Region(models.Model):
     region = models.TextField()
+    #como esta hecho para chile podriamos poner un ChoiceField de todas las regiones de chile
+    def __str__(self):
+        return self.region
+    
     
 
 class Comuna(models.Model):
     comuna = models.TextField()
-
+    def __str__(self):
+        return self.comuna
+    
 class Tipo_inmueble(models.Model):
     CHOICES = [
         ('Departamento', 'Departamento'),
@@ -35,7 +53,9 @@ class Tipo_inmueble(models.Model):
         ('Parcela', 'Parcela'),
     ]
     tipo = models.TextField(choices=CHOICES)
-
+    def __str__(self):
+        return self.tipo
+    
     """Departamento, Casa, Parcela"""
     """
     ID  tipo
@@ -52,14 +72,19 @@ class Tipo_usuario(models.Model):
         ('Arrendatario', 'Arrendatario')
     ]
     tipo = models.CharField(choices=CHOICES)
-
+    def __str__(self):
+        return self.tipo
+    
 class Perfil(models.Model):
     usuario = models.OneToOneField('auth.User',on_delete=models.CASCADE)
+    tipo_usuario = models.ForeignKey('Tipo_usuario', on_delete=models.CASCADE)
     rut = models.TextField()
     direccion = models.TextField()
     telefono = models.TextField()
     correo = models.EmailField()
     
+    def __str__(self):
+        return self.usuario.username
     
     
     """
---
+++ app/services.py
@@ -0,0 +1,11 @@
+from .models import Inmueble, Tipo_inmueble, User
+
+def get_all_inmubles():
+    pass
+
+def insertar_inmuble():
+    pass
+def actualizar_inmueble():
+    pass
+def borrar_inmueble():
+    pass
