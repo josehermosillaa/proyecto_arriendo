76d9c125aa7270a2d8135fb0e5614e9ff3e1b329
josehermosillaa <jose.hermosillaa@usach.cl>
2024-05-29 19:27:31 -0400

    get inmuebles, region

--- app/services.py
+++ app/services.py
@@ -29,3 +29,22 @@ def get_list_inmuebles(comuna):
         print(p.comuna)
         print(p.region)
         print(p.direccion)
+
+def get_inmuebles(region):
+    select = f"""
+    SELECT A.id,C.comuna, B.region, nombre_inmueble,direccion  FROM app_inmueble as A
+    INNER JOIN app_region as B
+	ON A.id_region_id = B.id
+    INNER JOIN app_comuna as C
+	ON A.id_comuna_id = C.id
+    WHERE C.comuna like '{region}'
+    """
+
+    query = Inmueble.objects.raw(select)
+
+    #crear el archivo con la salida
+    for p in query:
+        print(p.nombre_inmueble)
+        print(p.comuna)
+        print(p.region)
+        print(p.direccion)
