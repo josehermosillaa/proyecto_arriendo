0df9fce29877a319ffdfef58c9d05f22f8151e6e
josehermosillaa <jose.hermosillaa@usach.cl>
2024-05-29 19:25:33 -0400

    raw query

--- app/services.py
+++ app/services.py
@@ -9,3 +9,23 @@ def actualizar_inmueble():
     pass
 def borrar_inmueble():
     pass
+
+
+def get_list_inmuebles(comuna):
+    select = f"""
+    SELECT A.id,C.comuna, B.region, nombre_inmueble,direccion  FROM app_inmueble as A
+    INNER JOIN app_region as B
+	ON A.id_region_id = B.id
+    INNER JOIN app_comuna as C
+	ON A.id_comuna_id = C.id
+    WHERE C.comuna like '{comuna}'
+    """
+
+    query = Inmueble.objects.raw(select)
+
+    #crear el archivo con la salida
+    for p in query:
+        print(p.nombre_inmueble)
+        print(p.comuna)
+        print(p.region)
+        print(p.direccion)
