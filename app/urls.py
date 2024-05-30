33b1962b02646bc467ce76f848b85b8fb8c8d3f6
josehermosillaa <jose.hermosillaa@usach.cl>
2024-05-30 19:44:09 -0400

    register login and logout

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from .views import index
+from .views import index, register
+from django.contrib.auth.views import LoginView, LogoutView
+
 urlpatterns = [
-    path('',index),
+    path('',index, name='home'),
+    path('login/', LoginView.as_view(next_page='home'), name='login_url'),
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
+    path('register/', register, name='register')
+
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from .models import Inmueble
+from .forms import UserForm
 # Create your views here.
 def index(request):
     inmuebles = Inmueble.objects.all()
@@ -7,3 +8,15 @@ def index(request):
         'inmuebles': inmuebles
     }
     return render(request, 'index.html', context)
+    
+    
+def register(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        form = UserForm()
+        context = {'form':form}
+        return render(request,'registration/register.html', context)
